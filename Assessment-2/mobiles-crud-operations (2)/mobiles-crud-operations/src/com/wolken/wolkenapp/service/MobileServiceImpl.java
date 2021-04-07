d22274a5830c8537fd0446b317cfac72204964f7
meghashreemsl <81949646+meghashreemsl@users.noreply.github.com>
2021-04-07 23:25:35 +0530

    Add files via upload

---
+++ Assessment-2/mobiles-crud-operations (2)/mobiles-crud-operations/src/com/wolken/wolkenapp/service/MobileServiceImpl.java
@@ -0,0 +1,14 @@
+package com.wolken.wolkenapp.service;
+
+import com.wolken.wolkenapp.dto.MobileDTO;
+
+public class MobileServiceImpl implements MobileService {
+
+	@Override
+	public boolean validateandsave(MobileDTO mobileDTO) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
